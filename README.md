3fbcf53d3eda7e05bc903bd3fe20138e3ad0da98
Mikael Göransson <github@mgor.se>
2021-09-23 10:37:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grizzly Command Line Interface
+
+Documentation in [`biometria-se/grizzly`](https://github.com/Biometria-se/grizzly/docs/cli.md).
---
+++ setup.py
@@ -0,0 +1,58 @@
+import codecs
+import os
+
+from typing import List
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+def long_description() -> str:
+    with codecs.open('README.md', encoding='utf-8') as fd:
+        return fd.read()
+
+
+def install_requires() -> List[str]:
+    install_requires: List[str] = []
+    with codecs.open('requirements.txt', encoding='utf-8') as fd:
+        for line in fd.readlines():
+            install_requires.append(line.strip())
+
+    return install_requires
+
+
+def grizzly_cli_static_files() -> List[str]:
+    files: List[str] = []
+
+    base = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'grizzly_cli')
+
+    for path in Path(os.path.join(base, 'static')).rglob('*'):
+        if not path.is_file():
+            continue
+
+        files.append(str(path).replace(f'{base}/', ''))
+
+    return files
+
+
+setup(
+    name='grizzly-cli',
+    version='4.0.0',
+    description='Command line interface for grizzly',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/biometria-se/grizzly-cli',
+    author='Mikael Göransson',
+    author_email='mikael.goransson@biometria.se',
+    license='MIT',
+    packages=find_packages(),
+    package_data={
+        'grizzly_cli': grizzly_cli_static_files(),
+    },
+    python_requires='>=3.6',
+    install_requires=install_requires(),
+    entry_points={
+        'console_scripts': [
+            'grizzly-cli=grizzly_cli.cli:main',
+        ],
+    },
+)
